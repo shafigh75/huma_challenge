05fca82857f0cc700f8f34974c03def6f982907d
shafigh75 <shafigh75@gmail.com>
2024-09-13 16:14:34 +0200

    added docker swarm and traefik config

--- account-manager/server.js
+++ account-manager/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const http = require('http');
 const app = require('./app');
 
-const port = /*process.env.PORT || */5000;
+const port = /*process.env.PORT || */6000;
 
 const server = http.createServer(app);
 
